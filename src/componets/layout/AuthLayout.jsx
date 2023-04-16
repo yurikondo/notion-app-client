1469281123018ddaa396a3c2fa4fb1d39de52b57
yuri <yuri@example.com>
2023-04-17 08:28:03 +0900

    61. クライアントがすでにJWTを持っているかチェックするロジックを作ろう

--- src/componets/layout/AuthLayout.jsx
+++ src/componets/layout/AuthLayout.jsx
@@ -3,6 +3,7 @@ import { Container } from "@mui/system";
 import React, { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import notionLogo from "../../assets/images/notion-logo.png";
+import authUtils from "../../utils/authUtils";
 
 const AuthLayout = () => {
   const navigate = useNavigate();
@@ -11,6 +12,10 @@ const AuthLayout = () => {
   useEffect(() => {
     const checkAuth = async () => {
       //認証チェック
+      const isAuth = await authUtils.isAuthenticated();
+      if (isAuth) {
+        navigate("/");
+      }
     };
     checkAuth();
   }, [navigate]);
