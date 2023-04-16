bfc63b8c3b53351fe80857c42673e10aaa5ab6fc
yuri <yuri@example.com>
2023-04-16 17:38:25 +0900

    46. Axios interceptorsを利用してリクエストの前処理を設定

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,9 +1,26 @@
 /* axiosはAPIを叩くためのライブラリ */
 import axios from "axios";
+
 const BASE_URL = "http://localhost:4000/api/v1";
+
+//ローカルストレージからトークンを取得
+const getToken = () => {
+  localStorage.getItem("token");
+};
 
 /* カスタムでaxiosのインスタンスを作成(https://axios-http.com/docs/instance) */
 /* 追加できるオブジェクト一覧(https://axios-http.com/docs/req_config) */
 const axiosClient = axios.create({
   baseURL: BASE_URL,
+});
+
+/* APIを叩く前（リクエストを送る前）に前処理を捕まえる(https://axios-http.com/docs/interceptors) */
+axiosClient.interceptors.request.use(async (config) => {
+  return {
+    config,
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${getToken()}`, //リクエストヘッダにJWTをつけてサーバーに渡す
+    },
+  };
 });
