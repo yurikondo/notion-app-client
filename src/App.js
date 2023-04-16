7e7e2f19a431f18fc092f8f7051a44674121f27d
yuri <yuri@example.com>
2023-04-16 16:03:15 +0900

    39. react-router-domでルーティング設定をはじめよう

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthLayout from "./componets/layout/AuthLayout";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* AuthLayoutはLoginとRegisterに共通するコンポーネント */}
+        <Route path="/" element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/componets/layout/AuthLayout.jsx
+++ src/componets/layout/AuthLayout.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  return (
+    <div>
+      AuthLayout
+      <Outlet />
+    </div>
+  );
 };
 
 export default AuthLayout;
