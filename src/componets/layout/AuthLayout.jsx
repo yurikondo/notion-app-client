a736d0b0ad0f08b2b433f8257a3252e773581e0c
yuri <yuri@example.com>
2023-04-17 08:18:00 +0900

    60. ログイン用APIを準備して叩いてみよう

--- src/componets/layout/AuthLayout.jsx
+++ src/componets/layout/AuthLayout.jsx
@@ -1,10 +1,20 @@
 import { Box } from "@mui/material";
 import { Container } from "@mui/system";
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import notionLogo from "../../assets/images/notion-logo.png";
 
 const AuthLayout = () => {
+  const navigate = useNavigate();
+
+  //ページ遷移するたびに起動
+  useEffect(() => {
+    const checkAuth = async () => {
+      //認証チェック
+    };
+    checkAuth();
+  }, [navigate]);
+
   return (
     <div>
       {/* "component"プロパティに設定された値は、Material UIのコンポーネントのHTML要素を指定するために使用されます。 */}
