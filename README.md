19d20de6efcddbc7ba907cc137115bdd456e756b
yuri <yuri@example.com>
2023-04-16 16:29:19 +0900

    41. ThemeProviderを利用してアプリ全体のテーマを決定しよう

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 38. ログインや新規登録のコンポーネント（ファイル）を作成
 39. react-router-domをインストール・ルーティング設定
 40. Material UIをインストール
+41. CSSデフォルトのを初期化・ThemeProviderを利用してアプリ全体のテーマを決定(https://mui.com/material-ui/customization/theming/)
--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthLayout from "./componets/layout/AuthLayout";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { blue } from "@mui/material/colors";
 
 function App() {
+  const theme = createTheme({
+    palette: { primary: blue },
+  });
+
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* AuthLayoutはLoginとRegisterに共通するコンポーネント */}
-        <Route path="/" element={<AuthLayout />}>
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <BrowserRouter>
+          <Routes>
+            {/* AuthLayoutはLoginとRegisterに共通するコンポーネント */}
+            <Route path="/" element={<AuthLayout />}>
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </CssBaseline>
+    </ThemeProvider>
   );
 }
 
