072975fc3336dfad00c7af542f8d47761c5e838d
yuri <yuri@example.com>
2023-04-19 08:14:55 +0900

    71. userSliceを実装しよう

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./features/userSlice";
 
+//https://redux-toolkit.js.org/tutorials/quick-start#create-a-redux-store
 export const store = configureStore({
-  reducer: {},
+  reducer: { user: userReducer },
 });
