49471387781a50862b868bfaad561259923752c5
yuri <yuri@example.com>
2023-04-19 12:55:10 +0900

    79. Homeコンポーネントを完成させよう

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
 
 function Home() {
+  const { loading, setLoading } = useState(false);
+  const createMemo = () => {};
+
   return (
-    <div>Home</div>
-  )
+    <Box
+      sx={{
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <LoadingButton
+        variant="outlined"
+        onClick={() => createMemo}
+        loading={loading}
+      >
+        最初のメモを作成
+      </LoadingButton>
+    </Box>
+  );
 }
 
-export default Home
+export default Home;
