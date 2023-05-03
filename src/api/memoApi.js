fda93caa2a1d3b89f60142e2fb236509ddba1bac
yuri <yuri@example.com>
2023-05-03 12:06:00 +0900

    92. メモ内容更新用APIを構築してみよう

--- src/api/memoApi.js
+++ src/api/memoApi.js
@@ -4,6 +4,7 @@ const memoApi = {
   create: () => axiosClient.post("memo"),
   getAll: () => axiosClient.get("memo"),
   getOne: (id) => axiosClient.get(`memo/${id}`),
+  update: (id, params) => axiosClient.put(`memo/${id}`, params),
 };
 
 export default memoApi;
